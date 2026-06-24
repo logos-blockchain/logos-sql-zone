5e1760aebde8719003718216f9364139e75c7bf8
kashepavadan <dkashepava@gmail.com>
2026-06-24 11:14:19 -0400

    tutorial branch renewal

--- common/src/state.rs
+++ common/src/state.rs
@@ -25,40 +25,4 @@ pub trait ZoneState: Send {
     fn finalized(&self) -> &[Msg];
 }
 
-/// In-memory implementation of [`ZoneState`].
-#[derive(Default)]
-pub struct InMemoryZoneState {
-    published: Vec<Msg>,
-    finalized: Vec<Msg>,
-}
-
-impl ZoneState for InMemoryZoneState {
-    fn on_published(&mut self, info: &InscriptionInfo) {
-        self.published
-            .push(Msg::from_payload(info.this_msg, &info.payload));
-    }
-
-    fn on_finalized(&mut self, inscriptions: &[InscriptionInfo]) {
-        for info in inscriptions {
-            if let Some(i) = self
-                .published
-                .iter()
-                .position(|m| m.msg_id == info.this_msg)
-            {
-                self.published.remove(i);
-            }
-            if !self.finalized.iter().any(|m| m.msg_id == info.this_msg) {
-                self.finalized
-                    .push(Msg::from_payload(info.this_msg, &info.payload));
-            }
-        }
-    }
-
-    fn published(&self) -> &[Msg] {
-        &self.published
-    }
-
-    fn finalized(&self) -> &[Msg] {
-        &self.finalized
-    }
-}
+// Your code here
